1631ffcacae29b0ff78be4211420a8ca43e70697
GUDURU RAJU <rajuguduru13@gmail.com>
2023-03-24 10:58:19 +0530

    Little En-Big En

---
+++ c_prgms/LE_BE_U.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+union LE {
+    int i;
+    char c[4];
+};
+
+int main() {
+    union LE BE;
+
+    BE.i = 0x12345678;
+
+    printf("Little Endian: %x %x %x %x\n",BE.c[0], BE.c[1], BE.c[2], BE.c[3]);
+
+    int i;
+    char temp;
+    for (i = 0; i < 2; i++) {
+        temp = BE.c[i];
+        BE.c[i] = BE.c[3 - i];
+        BE.c[3 - i] = temp;
+    }
+
+	  printf("Big Endian: %x %x %x %x\n", BE.c[0], BE.c[1], BE.c[2], BE.c[3]);
+		printf("i=%x",BE.i);
+    return 0;
+}
+

9b7487bc5c27cbb99f3f718df676464bff05538d
GUDURU RAJU <rajuguduru13@gmail.com>
2023-03-27 15:30:39 +0530

    stringcat

---
+++ c_prgms/assignments_27_03_23/strcat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void mystrcat(char *d,const char *s)
+{
+	while(*d)
+	{
+		d++;
+	}
+	while(*d++=*s++);
+}
+int main()
+{
+	char s[100];
+	char d[100];
+	printf("enter the source string:\n");
+	scanf("%[^\n]s",s);
+	getchar();
+	printf("enter the destination string:\n");
+  	scanf("%[^\n]s",d);
+	mystrcat(d,s);
+	printf("%s",d);
+}
